74aa410d5877e980ed4f05b571a322258dcd19ff
Uzair <04uzair.muhammad@gmail.com>
2024-12-25 22:02:42 +0500

    Login Page Created

---
+++ project/frontend/biitOfficialWall.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+const biit = () =>{
+    return(
+        <View>
+            <Text>Hurray</Text>
+        </View>
+    )
+}
+export default biit
